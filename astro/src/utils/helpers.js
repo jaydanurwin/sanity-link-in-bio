d2f1b8a77624ff3f770840c3189a8d559c12db27
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-11-23 21:51:12 -0700

    updated to latest astro version

--- astro/src/utils/helpers.js
+++ astro/src/utils/helpers.js
@@ -3,8 +3,25 @@ import imageUrlBuilder from '@sanity/image-url';
 
 const builder = imageUrlBuilder(client);
 
-export function sanityGraphqlQuery(query) {
-    return `${process.env.SANITY_GRAPHQL_URL}?query=${query}`;
+// export function sanityGraphqlQuery(query) {
+//     return `${process.env.SANITY_GRAPHQL_URL}?query=${query}`;
+// }
+
+export async function getSanityContent({ query, variables = {} }) {
+  const { data } = await fetch(
+    `${import.meta.env.PUBLIC_SANITY_GRAPHQL_URL}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    },
+  ).then((response) => response.json());
+  return data;
 }
 
 export function getSanityImageUrl(source) {
